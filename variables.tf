bca2c09f264b33522db2cfb97f466907b7a45704
spbarch <spb@archgroup.com>
2025-05-12 15:11:30 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,44 +1,32 @@
-variable "storage_account_name" {
-  description = "The name of the storage account. Must be unique."
-  type        = string
-}
-
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the storage account."
+  description = "Name of the resource group"
   type        = string
 }
 
 variable "location" {
-  description = "The Azure location where the storage account should be created."
+  description = "Azure region where the storage account will be deployed"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Unique name for the storage account"
   type        = string
 }
 
 variable "account_tier" {
-  description = "The performance tier of the storage account. Possible values: Standard, Premium."
+  description = "Performance tier (Standard or Premium)"
   type        = string
   default     = "Standard"
 }
 
 variable "account_replication_type" {
-  description = "The replication type of the storage account. Possible values: LRS, GRS, ZRS, RAGRS, etc."
+  description = "Replication strategy (LRS, GRS, ZRS, etc.)"
   type        = string
   default     = "LRS"
 }
 
-variable "allow_blob_public_access" {
-  description = "Allows or disallows public access to blob containers."
-  type        = bool
-  default     = false
-}
-
-variable "enable_https_traffic_only" {
-  description = "Enforces HTTPS traffic only."
-  type        = bool
-  default     = true
-}
-
 variable "tags" {
-  description = "A map of tags to assign to the resource."
+  description = "Tags to assign to the resources"
   type        = map(string)
   default     = {}
 }
