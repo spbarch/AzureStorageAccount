7a1fa2fc5a1fd143e4d0bbd166bbb15c598dc401
spbarch <spb@archgroup.com>
2025-05-12 17:53:09 -0400

    Delete modules/storage-account directory

--- modules/storage-account/output.tf
+++
@@ -1,9 +0,0 @@
-output "storage_account_id" {
-  description = "The ID of the storage account."
-  value       = azurerm_storage_account.this.id
-}
-
-output "storage_account_primary_endpoint" {
-  description = "The primary endpoint for the storage account."
-  value       = azurerm_storage_account.this.primary_blob_endpoint
-}
