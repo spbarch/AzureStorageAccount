095c26175554cca5d0909f16e3be93d8066e53bd
spbarch <spb@archgroup.com>
2025-05-12 15:11:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
-provider "azurerm" {
-  features {}
-}
-
 module "storage_account" {
-  source = "./modules/storage-account"
+  source = "./modules/storage_account"
 
-  storage_account_name      = var.storage_account_name
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  account_tier              = var.account_tier
-  account_replication_type  = var.account_replication_type
-  allow_blob_public_access  = var.allow_blob_public_access
-  enable_https_traffic_only = var.enable_https_traffic_only
-  tags                      = var.tags
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  storage_account_name     = var.storage_account_name
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  tags                     = var.tags
 }
