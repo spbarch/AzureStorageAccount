9796dc75b372a2f85fff2ac7d4d099644143495c
spbarch <spb@archgroup.com>
2025-05-12 15:11:47 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "storage_account_id" {
-  description = "The ID of the storage account."
+  description = "ID of the created storage account"
   value       = module.storage_account.storage_account_id
 }
 
-output "storage_account_primary_endpoint" {
-  description = "The primary endpoint for the storage account."
-  value       = module.storage_account.storage_account_primary_endpoint
+output "primary_blob_endpoint" {
+  description = "Primary Blob service endpoint for the storage account"
+  value       = module.storage_account.primary_blob_endpoint
 }
